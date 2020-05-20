826d073f68047e23004aad69f277e497a2aacdf1
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-20 01:00:25 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 start cfiles/start.vbs
 echo server made by Spigot, Automated install with TyTech038's code
-java -jar -Xmx1G spigot.jar
+java -jar -Xmx1G spigot.jar nogui
 start cfiles/stop.vbs
 echo Bye!
